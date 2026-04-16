9505edc9f735485963b97153bd47b94a770b5e9e
maximeri <87888632+maximeri@users.noreply.github.com>
2026-04-16 13:39:39 +0800

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        if len(t) > len(s):
+            return 0
+
+        # 2D dp[i][j] 手裡拿著 s 的第 i 個字，目標是拼出 t 的前 j 個字，共有多少種方法
+
+        # dp[i+1][j+1] = dp[i][j] + dp[i-1][j] # skip + use
+
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+
+        for i in range(len(s)):
+            dp[i][0] = 1
+
+        for i in range(1, len(s)+1):
+            for j in range(1, len(t)+1):
+                dp[i][j] = dp[i-1][j]
+                if s[i-1] == t[j-1]:
+                    dp[i][j] += dp[i-1][j-1]
+
+        return dp[len(s)][len(t)]
+
