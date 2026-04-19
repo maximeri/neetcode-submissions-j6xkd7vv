75a1f6a6e2981812b2ca30cf8eee0757002aa827
maximeri <87888632+maximeri@users.noreply.github.com>
2026-04-19 20:28:42 +0800

    Add: edit-distance - submission-5

---
+++ Data Structures & Algorithms/edit-distance/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        # 2D
+    #     x a b
+    # x   0
+    # a   1
+    # a   2
+    # b   3
+
+        if len(word1) > len(word2):
+            word1, word2 = word2, word1
+
+        dp = [i for i in range(len(word1) + 1)]
+
+        for j in range(1, len(word2) + 1):
+            prev = dp[0] # init left_right
+            dp[0] = j
+            for i in range(1, len(word1) + 1):
+                temp = dp[i]
+                if word1[i-1] == word2[j-1]:
+                    dp[i] = prev
+                else:
+                    dp[i] = 1 + min(
+                        dp[i-1],    # Delete
+                        dp[i],    # Insert
+                        prev   # Replace
+                    )
+                prev = temp # udpate left_right
+
+        return dp[len(word1)]
+
+
+
+            
