dd6e2fc6e905bfed148f5cf3f2c48cadf965a70a
maximeri <87888632+maximeri@users.noreply.github.com>
2026-04-21 19:37:11 +0800

    Add: shortest-common-supersequence - submission-6

---
+++ Data Structures & Algorithms/shortest-common-supersequence/submission-6.py
@@ -0,0 +1,46 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
+        dp = [[0] * (n + 1) for _ in range(m+1)]
+       
+        # LCS 兩個空字串的公共子序列長度
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                # same
+                if str1[i-1] == str2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        # SCS
+        res = []
+        i, j = m, n
+        while i > 0 and j > 0:
+            if str1[i-1] == str2[j-1]:
+                res.append(str1[i-1])
+                i -= 1
+                j -= 1
+            else:
+                if dp[i-1][j] >  dp[i][j-1]:
+                    res.append(str1[i-1])
+                    i-=1
+                else:
+                    res.append(str2[j-1])
+                    j-=1
+
+        while i > 0:
+            res.append(str1[i-1])
+            i -= 1
+
+        while j > 0:
+            res.append(str2[j-1])
+            j -= 1
+
+        res.reverse()
+
+        return "".join(res)
+            
+
+
+        
+
